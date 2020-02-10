2c8af64a22ead1d5d97ac66b32357d44343b9a39
peyo <peteryyoon@gmail.com>
2020-02-10 11:45:55 -0800

    removed api key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW',
+  API_KEY: 'ADD API KEY HERE',
 }
